2e4b75428b57a67afcd099b1b9220fb56127ccbd
Archmagoz <0josegabrielmatos0@gmail.com>
2026-03-17 21:29:00 -0300

    modified:   src/pdfimgextract/cli.py

--- src/pdfimgextract/cli.py
+++ src/pdfimgextract/cli.py
@@ -1,12 +1,13 @@
 from colorama import init as colorama_init
 
 from pdfimgextract.args import get_args
+from pdfimgextract.datamodels import Args
 from pdfimgextract.extract import extract_images_parallel
 
 
 def main() -> int:
     colorama_init()
 
-    args = get_args()
+    args: Args = get_args()
 
     return extract_images_parallel(args)
